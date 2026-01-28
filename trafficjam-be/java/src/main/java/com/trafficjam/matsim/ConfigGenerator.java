061dd202fdf1698f1189cffbf1f3da4d668e78e0
Clancy DeSilva <desilva.clancy@gmail.com>
2026-01-28 17:33:12 +0000

    feat: Java MATSim skeleton code added

---
+++ trafficjam-be/java/src/main/java/com/trafficjam/matsim/ConfigGenerator.java
@@ -0,0 +1,13 @@
+package com.trafficjam.matsim;
+
+/**
+ * Generates MatSim configuration files from templates
+ * TODO: Implement config generation
+ */
+public class ConfigGenerator {
+
+    public String generateConfig(String networkXml, String plansXml) {
+        // TODO: Generate MatSim config.xml from template
+        return null;
+    }
+}
---
+++ trafficjam-be/java/src/main/java/com/trafficjam/matsim/MatsimRunner.java
@@ -0,0 +1,12 @@
+package com.trafficjam.matsim;
+
+/**
+ * Main class for running MatSim simulations
+ * TODO: Implement MatSim simulation runner
+ */
+public class MatsimRunner {
+
+    public void runSimulation(String configPath) {
+        // TODO: Load MatSim config and run simulation
+    }
+}
