6325e657291218006d5a50c6864db4d2f0b6e021
Clancy DeSilva <desilva.clancy@gmail.com>
2026-01-28 17:33:12 +0000

    feat: Java MATSim skeleton code added

---
+++ trafficjam-be/java/src/main/java/com/trafficjam/matsim/EventHandler.java
@@ -0,0 +1,12 @@
+package com.trafficjam.matsim;
+
+/**
+ * Custom event handler to capture MatSim events
+ * TODO: Implement event handling
+ */
+public class EventHandler {
+
+    public void handleEvent(Object event) {
+        // TODO: Capture and process MatSim events
+    }
+}
