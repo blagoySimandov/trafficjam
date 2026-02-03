e17b23d18204aac03c4f230451f32c5501d29504
blagoySimandov <baggy.simandoff@gmail.com>
2026-02-03 15:26:22 +0000

    chore: refacotr

---
+++ map-data-service/constants.py
@@ -0,0 +1,22 @@
+LINK_TAG_KEYS = frozenset(
+    {"highway", "lanes", "maxspeed", "oneway", "name", "ref", "surface"}
+)
+
+BUILDING_TAG_KEYS = frozenset(
+    {
+        "building",
+        "name",
+        "amenity",
+        "shop",
+        "addr:street",
+        "addr:housenumber",
+        "height",
+        "building:levels",
+    }
+)
+
+ROUTE_TAG_KEYS = frozenset(
+    {"route", "ref", "name", "operator", "network", "from", "to"}
+)
+
+VALID_MEMBER_ROLES = frozenset({"", "forward", "backward"})
