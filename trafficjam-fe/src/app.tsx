beb4a8bd955d0b9fabf1fe9dabc5d08e9e42d8c0
Mert Milenov <mertimladenov@gmail.com>
2026-02-08 20:35:49 +0000

    Adde some additional stuff needed for the agent attributes

--- trafficjam-fe/src/app.tsx
+++ trafficjam-fe/src/app.tsx
@@ -1,15 +1,5 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Visualizer } from "./presentation/visualizer";
 import { Editor } from "./presentation/editor";
 
-const queryClient = new QueryClient();
-
 export default function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      {/* disabled the next line since it makes it easuer yo chagne between the two... */}
-      {/*eslint-disable-next-line*/}
-      {true ? <Visualizer /> : <Editor />}
-    </QueryClientProvider>
-  );
+  return <Editor />;
 }
--- trafficjam-fe/src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-export { useOSMImport } from "./use-osm-import";
-export { useMapInteractions } from "./use-map-interactions";
-export {
-  useNetworkInteraction,
-  INTERACTIVE_LAYER_IDS,
-} from "./use-network-interaction";
