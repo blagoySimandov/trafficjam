875520bfe74f0c9144c2a949cf2b97a06e37a457
carlosguellr <145341055+carlosguellr@users.noreply.github.com>
2026-02-09 18:15:56 +0100

    TRA-75 [Meditor] Implement visual feedback (#43)
    * MVP of Map editor functionality. Currently allows to view nodes and allows the user to move them around
    
    * Fixed road export
    
    * Lint error fix ?
    
    * Lint fix for OSM el when creating nodes
    
    * Removed unused functionality of Editor Mode Spike. Made changes on comments
    
    * Fixed lint issue. Removed remaining unused utility.
    
    * Made changes to use-node-drag. Changed to useEffect to listen directly for Mapbox's event sysystem to access the map directly. Makes it easier to get nodes - queryRenderedFeatures()
    
    * Fixed lint any type for MapMouseEvent
    
    * Fixed dragging functionality on map-view
    
    * no more greek letters
    
    * idk what i'm doing anymore. snapping breaks
    
    * Merged changes from main and resolved conflicts. +Changed UI
    
    * Fixed lint error. Geometry is now const
    
    * Added snapping functionality. Nodes snap within a couple of meters when dragging
    
    * Improved UX: Dragged node only snaps on mouse up if you are on top (close) to another node.
    
    * Now remove empty nodes upon merging
    
    * merge
    
    * Fix lint frozen lockfile issue (maybe???)
    
    * Fix lint frozen lockfile issue (maybe???)
    
    Removed big oopsie (node_modules)
    
    * Hopefully now i actually fixed it
    
    * Went back to our version of react
    
    * Added visual feedback for dragging nodes. Color changes
    
    * Fixed snapping intermediary nodes
    
    * Moved node drag styles to constants in colors.ts
    
    * Created a use-node-layer-style hook for changing node color (visual feedback
    
    ---------
    
    Co-authored-by: blagoySimandov <baggy.simandoff@gmail.com>

--- trafficjam-fe/src/presentation/editor/components/layers/node-layer.tsx
+++ trafficjam-fe/src/presentation/editor/components/layers/node-layer.tsx
@@ -1,22 +1,25 @@
-import { useMemo } from "react";
 import { Source, Layer } from "react-map-gl";
 import type { Network } from "../../../../types";
-import { nodeToGeoJSON } from "../../../../utils";
-import { NODE_CIRCLE_LAYER } from "../../../../constants";
+import { useNodeLayerStyle } from "../../hooks/use-node-layer-style";
 
 interface NodeLayerProps {
   network: Network;
   editorMode: boolean;
+  draggedNodeId: string | null;
 }
 
-export function NodeLayer({ network, editorMode }: NodeLayerProps) {
-  const geojson = useMemo(() => nodeToGeoJSON(network), [network]);
+export function NodeLayer({
+  network,
+  editorMode,
+  draggedNodeId,
+}: NodeLayerProps) {
+  const { geojson, layerStyle } = useNodeLayerStyle(network, draggedNodeId);
 
   if (!editorMode) return null;
 
   return (
     <Source id="nodes" type="geojson" data={geojson}>
-      <Layer {...NODE_CIRCLE_LAYER} />
+      <Layer {...layerStyle} />
     </Source>
   );
 }
