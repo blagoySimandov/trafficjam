9f00a98b60e13670211faed12531cfeb6eea7c7b
blagoySimandov <baggy.simandoff@gmail.com>
2026-02-12 19:27:18 +0000

    fix: use overpass again

--- map-data-service/config.py
+++ map-data-service/config.py
@@ -4,11 +4,14 @@ from functools import lru_cache
 
 class Settings(BaseSettings):
     debug: bool = False
-    
+
+    # "gcs" or "overpass"
+    data_source: str = "overpass"
+
     # Google Cloud Storage settings
-    gcs_bucket_name: str = "trafficjam-network-data"
-    gcs_network_file: str = "network.osm.json"
-    
+    gcs_bucket_name: str = "tj-mapdata"
+    gcs_network_file: str = "ireland-and-northern-ireland-260210.osm.json"
+
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
