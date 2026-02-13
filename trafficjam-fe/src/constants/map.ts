41263896bad2e9686d23920ce89c57dca26f14b3
carlosguellr <carlosguellreverter@gmail.com>
2026-02-13 12:04:07 +0000

    Created use-node-add hook to add nodes anywhere from zoom 17

--- trafficjam-fe/src/constants/map.ts
+++ trafficjam-fe/src/constants/map.ts
@@ -5,6 +5,7 @@ import { NODE_LAYER_ID } from "./node-layers";
 export const DEFAULT_CENTER: [number, number] = [-8.47, 51.9];
 export const DEFAULT_ZOOM = 15;
 export const MIN_IMPORT_ZOOM = 14;
+export const MIN_EDIT_ZOOM = 17;
 export const MAP_STYLE = "mapbox://styles/mapbox/light-v11";
 export const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN || "";
 export const INTERACTIVE_LAYER_IDS = [
