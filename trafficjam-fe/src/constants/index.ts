a7dc64687eae0828faf89065892be1aa757d4d3b
carlosguellr <carlosguellreverter@gmail.com>
2026-02-16 18:33:10 +0000

    Link-attribute-panel folder re-structure

--- trafficjam-fe/src/constants/index.ts
+++ trafficjam-fe/src/constants/index.ts
@@ -1,5 +1,6 @@
 export * from "./building";
 export * from "./colors";
+export * from "./highway-types";
 export * from "./layer-styles";
 export * from "./map";
 export * from "./road-styles";
---
+++ trafficjam-fe/src/presentation/link-attribute-panel/constants.ts
@@ -0,0 +1,22 @@
+export const LANE_OPTIONS = [1, 2, 3, 4, 5, 6];
+
+export const MAXSPEED_OPTIONS = [
+  { value: "", label: "Not specified" },
+  { value: "20", label: "20 km/h" },
+  { value: "30", label: "30 km/h" },
+  { value: "40", label: "40 km/h" },
+  { value: "50", label: "50 km/h" },
+  { value: "60", label: "60 km/h" },
+  { value: "70", label: "70 km/h" },
+  { value: "80", label: "80 km/h" },
+  { value: "90", label: "90 km/h" },
+  { value: "100", label: "100 km/h" },
+  { value: "110", label: "110 km/h" },
+  { value: "120", label: "120 km/h" },
+  { value: "130", label: "130 km/h" },
+];
+
+export const ONEWAY_OPTIONS = [
+  { value: "false", label: "No" },
+  { value: "true", label: "Yes" },
+];
