f5395f9a908ebd6fff4650379a08af61959ec5eb
Mert Milenov <mertimladenov@gmail.com>
2026-02-16 15:27:51 +0000

    Everything now uses haversine calculations

--- trafficjam-be/utils/geo.py
+++ trafficjam-be/utils/geo.py
@@ -4,16 +4,10 @@ from haversine import haversine, Unit
 
 
 def calculate_area_wgs84(bounds: Dict[str, float]) -> float:
+    """Calculate area in km² from WGS84 (EPSG:4326) bounds using haversine formula."""
     north, south = bounds["north"], bounds["south"]
     east, west = bounds["east"], bounds["west"]
 
     height_km = haversine((south, west), (north, west), unit=Unit.KILOMETERS)
     width_km = haversine((south, west), (south, east), unit=Unit.KILOMETERS)
     return height_km * width_km
-
-
-def calculate_area_projected(bounds: Dict[str, float]) -> float:
-    width_m = bounds["east"] - bounds["west"]
-    height_m = bounds["north"] - bounds["south"]
-    area_m2 = width_m * height_m
-    return area_m2 / 1_000_000
