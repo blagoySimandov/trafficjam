d10e710fab1c6567c16c67ef46e08a2892dbe789
Blagoy Simandoff <54207300+blagoySimandov@users.noreply.github.com>
2026-02-19 14:02:54 +0000

    chore: rename +add gitignore (#78)
    * chore: rename +add gitignore
    
    * lint
    
    * lint 2

--- simengine/run-server.sh
+++ simengine/run-server.sh
@@ -4,7 +4,7 @@
 LOG_FILE="server-output.log"
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
-echo "=== Server started at $TIMESTAMP ===" > "$LOG_FILE"
+echo "=== Server started at $TIMESTAMP ===" >"$LOG_FILE"
 
 echo "Starting Spring Boot server..."
 echo "All output will be written to: $LOG_FILE"
@@ -12,5 +12,4 @@ echo "Press Ctrl+C to stop the server"
 echo ""
 
 # Run Maven and log output
-cd ./java
 mvn spring-boot:run 2>&1 | tee -a "../$LOG_FILE"
