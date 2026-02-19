9a7b0ae04e2d2ee563b65f7732e20c4514ed1c46
Mert Milenov <100515580+MrORE0@users.noreply.github.com>
2026-02-19 17:40:33 +0000

    Broke map-data-service into modular files and added tests (#81)

---
+++ trafficjam-fe/src/api/map-data-service/index.ts
@@ -0,0 +1,9 @@
+export { mapDataApi, fetchNetworkData } from "./client";
+export { mapNetworkResponse } from "./decoder";
+export type {
+  ApiTrafficNode,
+  ApiTrafficLink,
+  ApiBuilding,
+  ApiTransportRoute,
+  ApiNetworkResponse,
+} from "./types";
