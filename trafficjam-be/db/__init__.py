bab9e1b1e9ccbc6ccb3d0d2281701e0f6e11c502
Clancy DeSilva <desilva.clancy@gmail.com>
2026-02-25 18:57:24 +0000

    feat: added scenario repo, creates db sesh

--- trafficjam-be/db/__init__.py
+++ trafficjam-be/db/__init__.py
@@ -1,5 +1,7 @@
 from db.database import engine, async_session_factory
 from db.models import Run, RunStatus, Scenario
 from db.repository import RunRepository
+from db.scenario_repository import ScenarioRepository
 
-__all__ = ["engine", "async_session_factory", "Run", "RunStatus", "Scenario", "RunRepository"]
+__all__ = ["engine", "async_session_factory", "Run", "RunStatus", "Scenario", "RunRepository", "ScenarioRepository"]
+
